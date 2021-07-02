77c513a93e651197a8c462346302df5ba093b0b8
BrenaSim0es <52006594+BrenaSim0es@users.noreply.github.com>
2021-07-01 22:27:16 -0300

    Create Item.cs

---
+++ Item.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+    public class item
+    {
+      public string Nome;
+      public string Descricao;
+      public int Valor;
+      public string Cat;
+
+      public item(string nome, string Descricao, int Valor, string Cat);
+      {
+          this.Nome= nome;
+          this.Descricao= descricao;
+          this.Valor= Valor;
+          this.Cat= Cat;
+      }
+    }
